d9fe24e650c7a7fa4013a740036daa4b368920d4
damandeep611 <damandeep611@gmail.com>
2023-11-18 16:16:05 +0530

    files added

---
+++ script.js
@@ -0,0 +1,55 @@
+// mobile navigation
+const primaryHeader = document.querySelector('.primary-header');
+const navToggle = document.querySelector('.mobile-nav-toggle');
+const primaryNav = document.querySelector('.primary-navigation');
+
+navToggle.addEventListener('click', () => {
+  primaryNav.hasAttribute("data-visible")
+  ? navToggle.setAttribute("aria-expanded", false)
+  : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute('data-visible');
+  primaryHeader.toggleAttribute('data-overlay');
+})
+
+
+
+// typing effect 
+var typed = new Typed("#typed", {
+  strings:['data driven algorithms', 'and data driven analysis'],
+  typeSpeed: 45,
+  smartBackspace: true,
+  backspeed: 55,
+  loop: true,
+}); 
+
+ // Init Swiper
+  
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
